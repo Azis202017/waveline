a2e0ebac77bb88d8dcfabf1e8a306229e7b4739e
Azis202017 <azisa6980@gmail.com>
2023-11-15 09:52:09 +0700

    Add theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,13 @@ import 'package:waveline/app/theme/theme.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
-      title: "Waveline",
+      title: "Waveline", 
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-      theme:theme
+      theme: theme,
     ),
   );
 }
