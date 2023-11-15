a7acec8b6303210d08f438d7a7914d0445cc1ab6
Azis202017 <azisa6980@gmail.com>
2023-11-15 09:53:10 +0700

    Add services for auth

--- lib/app/services/auth_service.dart
+++ lib/app/services/auth_service.dart
@@ -1,15 +1,23 @@
+import 'dart:convert';
+
+import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:waveline/app/global/constanta.dart';
 class AuthService {
   Future<bool> login({required String email, required String password}) async {
+    GetStorage storage = GetStorage();
     try{  
-      Uri url = Uri.parse('$baseUrl/login');
+      Uri url = Uri.parse('$baseUrl/login-user');
       Map<String,dynamic> body = {
         "email" : email,
         "password" : password
       };
       var response = await http.post(url, body : body);
+      print(response.body);
+
       if(response.statusCode == 200) {
+        String token = json.decode(response.body)['access_token'];
+        storage.write('token', 'Bearer $token');
         return true;
       }
       return false;
@@ -17,21 +25,5 @@ class AuthService {
       return throw Exception(e);
     }
   }
-  Future<bool> register({required String email, required String password, required String fullName}) async{
-    try{
-      Uri url = Uri.parse('$baseUrl/register');
-      Map<String,dynamic> body = {
-        "email" : email, 
-        "password" : password,
-        "fullName" : fullName
-      };
-      var response =  await http.post(url, body: body);
-      if(response.statusCode == 200) {
-        return true;
-      }
-      return false;
-    }catch(e){
-      return throw Exception(e);
-    }
-  }
+ 
 }
