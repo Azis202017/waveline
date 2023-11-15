beeac12a7812618955b695ffb18d037c2bcd61e8
Azis202017 <azisa6980@gmail.com>
2023-11-15 09:52:49 +0700

    Add fitur login

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -7,14 +7,40 @@ class LoginController extends GetxController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   bool isLogin = false;
-  void login() async{
-    isLogin = await AuthService().login(email: emailController.text, password: passwordController.text);
-    if(isLogin){
-      Get.offAllNamed(Routes.HOME);
-    }else {
-      
+  bool isObsecurePassword = true;
+  final formKey = GlobalKey<FormState>();
+  void login() async {
+    isLogin = await AuthService().login(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+    if (formKey.currentState!.validate()) {
+      if (isLogin) {
+        Get.offAllNamed(Routes.BOTTOM_NAV);
+      } else {}
     }
-    
+  }
 
+  void showPassword() {
+    isObsecurePassword = !isObsecurePassword;
+    update();
+  }
+
+  String? isValidPassword(String? password) {
+    
+    if (password!.length < 7) {
+      return "You have the password 7";
+    }
+    return null;
+  }
+
+  String? isValidEmail(String? email) {
+     if (email!.isEmpty) {
+      return "You have the password 7";
+    }
+    if (!email.contains("@")) {
+      return "Valid email use @";
+    }
+    return null;
   }
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:waveline/app/theme/color.dart';
+import 'package:waveline/app/theme/font.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -8,34 +10,156 @@ class LoginView extends GetView<LoginController> {
   const LoginView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(),
-              TextFormField(),
-              const SizedBox(
-                height: 40,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Login'),
+    return GetBuilder<LoginController>(builder: (_) {
+      return SafeArea(
+        child: Scaffold(
+          body: Form(
+            key: controller.formKey,
+            child: SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 32,
                 ),
-              )
-            ],
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          'assets/img/logo-biru.png',
+                          width: 77,
+                          height: 35,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          'Sign in',
+                          textAlign: TextAlign.center,
+                          style: h3Bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Center(
+                        child: Text(
+                          'Hi! Welcome back, predict your stroke with waveline',
+                          style: h5Regular.copyWith(
+                            color: fontAbu,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 42,
+                      ),
+                      Text(
+                        'Email',
+                        textAlign: TextAlign.center,
+                        style: bodyRegular,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: controller.emailController,
+                        validator: controller.isValidEmail,
+                        decoration: InputDecoration(
+                          prefixIconConstraints: const BoxConstraints(
+                            minWidth: 16,
+                            minHeight: 16,
+                          ),
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                            ),
+                            child: Image.asset(
+                              'assets/img/email.png',
+                              width: 24,
+                              height: 24,
+                              color: fontAbu,
+                            ),
+                          ),
+                          hintText: 'example@gmail.com',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        'Password',
+                        textAlign: TextAlign.center,
+                        style: bodyRegular,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      TextFormField(
+                        controller: controller.passwordController,
+                        obscureText: controller.isObsecurePassword,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: controller.isValidPassword,
+                        decoration: InputDecoration(
+                          hintText: '***********',
+                          prefixIconConstraints: const BoxConstraints(
+                            minWidth: 16,
+                            minHeight: 16,
+                          ),
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                            ),
+                            child: Image.asset(
+                              'assets/img/password.png',
+                              width: 24,
+                              height: 24,
+                              color: fontAbu,
+                            ),
+                          ),
+                          suffixIconConstraints: const BoxConstraints(
+                            minWidth: 16,
+                            minHeight: 16,
+                          ),
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                            ),
+                            child: GestureDetector(
+                              onTap: controller.showPassword,
+                              child: Image.asset(
+                                'assets/img/${controller.isObsecurePassword ? "nlp" : "lp"}.png',
+                                width: 24,
+                                height: 24,
+                                color: fontAbu,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: controller.login,
+                          child: const Text('Login'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
-      ),
-      
-    );
+      );
+    });
   }
 }
