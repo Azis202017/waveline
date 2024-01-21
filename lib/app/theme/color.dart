8ce31d66d7d2e10a5265249033d67d33afbc0cd7
Azis202017 <azisa6980@gmail.com>
2024-01-21 23:22:24 +0700

    added primary, secondaryColor3, secondaryColor4, background color name card, background color age, background gender, background weight

--- lib/app/theme/color.dart
+++ lib/app/theme/color.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
-
-const Color primary = Color.fromARGB(255, 27, 137, 234);
+const Color primary = Color.fromARGB(255, 40, 134, 216);
 const Color secondaryColor = Color.fromARGB(180, 27, 137, 234);
+const Color secondaryColor3 = Color.fromARGB(23, 84, 193, 251);
+const Color secondaryColor4 = Color(
+  0XFF54C1FB,
+);
+
 const Color backgroundColor = Color(0xffFBFBFB);
+const Color backgroundColorNameCard = Color.fromRGBO(70, 199, 253, 0.03);
+const Color backgroundColorAge = Color.fromRGBO(70, 144, 253, 0.03);
+const Color backgroundGender = Color.fromRGBO(240, 18, 131, 0.03);
+const Color backgroundWeight = Color.fromRGBO(245, 152, 38, 0.03);
 
 const Color accentColor = Color(0xffF4FBF7);
 
@@ -13,7 +21,7 @@ const Color font2 = Color(0xff5D5F60);
 const Color black = Colors.black;
 const Color font = Color(0xff1E1E1E);
 const Color secondaryColor2 = Color(0xffF4FBF7);
-const Color fontAbu = Color(0xff686868);
+const Color fontAbu = Color(0xffA1A8B0);
 const Color whiteColor = Colors.white;
 const Color errorColor = Colors.red;
 const Color greyColor = Color(0xffAEB2B7);
