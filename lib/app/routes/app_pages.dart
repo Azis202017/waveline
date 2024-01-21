8dcf7964624300c220826cc8af543ebe9cd33863
Azis202017 <azisa6980@gmail.com>
2024-01-21 23:20:51 +0700

    add some pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,15 +1,31 @@
-// ignore_for_file: constant_identifier_names
-
 import 'package:get/get.dart';
 
+import '../modules/bluetooth_scan/bindings/bluetooth_scan_binding.dart';
+import '../modules/bluetooth_scan/views/bluetooth_scan_view.dart';
+import '../modules/bottom_nav/bindings/bottom_nav_binding.dart';
+import '../modules/bottom_nav/views/bottom_nav_view.dart';
+import '../modules/detail_record/bindings/detail_record_binding.dart';
+import '../modules/detail_record/views/detail_record_view.dart';
+import '../modules/feedback/bindings/feedback_binding.dart';
+import '../modules/feedback/views/feedback_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/onboarding/bindings/onboarding_binding.dart';
+import '../modules/onboarding/views/onboarding_view.dart';
+import '../modules/profile/bindings/profile_binding.dart';
+import '../modules/profile/views/profile_view.dart';
+import '../modules/record/bindings/record_binding.dart';
+import '../modules/record/views/record_view.dart';
 import '../modules/register/bindings/register_binding.dart';
 import '../modules/register/views/register_view.dart';
 import '../modules/splash_screen/bindings/splash_screen_binding.dart';
 import '../modules/splash_screen/views/splash_screen_view.dart';
+import '../modules/started_page/bindings/started_page_binding.dart';
+import '../modules/started_page/views/started_page_view.dart';
+
+// ignore_for_file: constant_identifier_names
 
 part 'app_routes.dart';
 
@@ -38,6 +54,46 @@ class AppPages {
       name: _Paths.SPLASH_SCREEN,
       page: () => const SplashScreenView(),
       binding: SplashScreenBinding(),
+    ),
+    GetPage(
+      name: _Paths.BLUETOOTH_SCAN,
+      page: () => const BluetoothScanView(),
+      binding: BluetoothScanBinding(),
+    ),
+    GetPage(
+      name: _Paths.BOTTOM_NAV,
+      page: () => const BottomNavView(),
+      binding: BottomNavBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => const ProfileView(),
+      binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: _Paths.FEEDBACK,
+      page: () => const FeedbackView(),
+      binding: FeedbackBinding(),
+    ),
+    GetPage(
+      name: _Paths.ONBOARDING,
+      page: () => const OnboardingView(),
+      binding: OnboardingBinding(),
+    ),
+    GetPage(
+      name: _Paths.STARTED_PAGE,
+      page: () => const StartedPageView(),
+      binding: StartedPageBinding(),
+    ),
+    GetPage(
+      name: _Paths.RECORD,
+      page: () => const RecordView(),
+      binding: RecordBinding(),
+    ),
+    GetPage(
+      name: _Paths.DETAIL_RECORD,
+      page: () => const DetailRecordView(),
+      binding: DetailRecordBinding(),
     ),
   ];
 }
