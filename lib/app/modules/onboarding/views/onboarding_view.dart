5b5907315de60ed97203ddd3623c147a63ddef58
Azis202017 <azisa6980@gmail.com>
2024-01-21 23:17:28 +0700

    introduction screen onboarding

--- lib/app/modules/onboarding/views/onboarding_view.dart
+++ lib/app/modules/onboarding/views/onboarding_view.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:waveline/app/theme/font.dart';
 
+import '../../../theme/color.dart';
 import '../controllers/onboarding_controller.dart';
 
 class OnboardingView extends GetView<OnboardingController> {
-  const OnboardingView({Key? key}) : super(key: key);
+  const OnboardingView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('OnboardingView'),
-        centerTitle: true,
-      ),
-      body: const Center(
+    return IntroductionScreen(
+      key: controller.introKey,
+      onDone: controller.onIntroEnd,
+      pages: controller.pageViewModel,
+      showDoneButton: true,
+      done: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 18,
+        ),
+        decoration: BoxDecoration(
+          color: primary,
+          borderRadius: BorderRadius.circular(
+            16,
+          ),
+        ),
         child: Text(
-          'OnboardingView is working',
-          style: TextStyle(fontSize: 20),
+          'Done',
+          style: h6SemiBold.copyWith(
+            color: whiteColor,
+          ),
+        ),
+      ),
+      next: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 18,
+        ),
+        decoration: BoxDecoration(
+          color: primary,
+          borderRadius: BorderRadius.circular(
+            16,
+          ),
+        ),
+        child: Text(
+          'Next',
+          style: h6SemiBold.copyWith(
+            color: whiteColor,
+          ),
+        ),
+      ),
+      showNextButton: true,
+      showBackButton: false,
+      showSkipButton: true,
+      skip: Container(
+        child: Text(
+          'SKIP',
+          style: h6SemiBold.copyWith(
+            color: primary,
+          ),
         ),
       ),
     );
