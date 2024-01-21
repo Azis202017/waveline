bed349741cd6e17ede20d5e3eb25f2acd5f71577
Azis202017 <azisa6980@gmail.com>
2024-01-21 23:21:17 +0700

    added bluetooth scan view, bottom nav, profile, feedback, onboarding ,started page, record, detail record

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -9,6 +9,14 @@ abstract class Routes {
   static const LOGIN = _Paths.LOGIN;
   static const REGISTER = _Paths.REGISTER;
   static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
+  static const BLUETOOTH_SCAN = _Paths.BLUETOOTH_SCAN;
+  static const BOTTOM_NAV = _Paths.BOTTOM_NAV;
+  static const PROFILE = _Paths.PROFILE;
+  static const FEEDBACK = _Paths.FEEDBACK;
+  static const ONBOARDING = _Paths.ONBOARDING;
+  static const STARTED_PAGE = _Paths.STARTED_PAGE;
+  static const RECORD = _Paths.RECORD;
+  static const DETAIL_RECORD = _Paths.DETAIL_RECORD;
 }
 
 abstract class _Paths {
@@ -17,4 +25,12 @@ abstract class _Paths {
   static const LOGIN = '/login';
   static const REGISTER = '/register';
   static const SPLASH_SCREEN = '/splash-screen';
+  static const BLUETOOTH_SCAN = '/bluetooth-scan';
+  static const BOTTOM_NAV = '/bottom-nav';
+  static const PROFILE = '/profile';
+  static const FEEDBACK = '/feedback';
+  static const ONBOARDING = '/onboarding';
+  static const STARTED_PAGE = '/started-page';
+  static const RECORD = '/record';
+  static const DETAIL_RECORD = '/detail-record';
 }
