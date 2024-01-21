225ea29285855948a4f4e23b1e8617fb9b9f3922
Azis202017 <azisa6980@gmail.com>
2024-01-21 23:22:50 +0700

    change font to poppins

--- lib/app/theme/font.dart
+++ lib/app/theme/font.dart
@@ -3,233 +3,233 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'color.dart';
 
-TextStyle h1Regular = GoogleFonts.inter(
+TextStyle h1Regular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 32,
 );
-TextStyle h2Regular = GoogleFonts.inter(
+TextStyle h2Regular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 24,
 );
-TextStyle h3Regular = GoogleFonts.inter(
+TextStyle h3Regular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 20,
 );
-TextStyle h4Regular = GoogleFonts.inter(
+TextStyle h4Regular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 16,
 );
-TextStyle h5Regular = GoogleFonts.inter(
+TextStyle h5Regular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 12,
 );
 
-TextStyle h6Regular = GoogleFonts.inter(
+TextStyle h6Regular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 13,
 );
 
-TextStyle h1Bold = GoogleFonts.inter(
+TextStyle h1Bold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 32,
 );
-TextStyle h2Bold = GoogleFonts.inter(
+TextStyle h2Bold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 24,
 );
-TextStyle h3Bold = GoogleFonts.inter(
+TextStyle h3Bold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 20,
 );
-TextStyle h4Bold = GoogleFonts.inter(
+TextStyle h4Bold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 16,
 );
-TextStyle h5Bold = GoogleFonts.inter(
+TextStyle h5Bold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 15,
 );
-TextStyle h6Bold = GoogleFonts.inter(
+TextStyle h6Bold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 13,
 );
 
-TextStyle h1Medium = GoogleFonts.inter(
+TextStyle h1Medium = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: font,
   fontSize: 32,
 );
-TextStyle h2Medium = GoogleFonts.inter(
+TextStyle h2Medium = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: font,
   fontSize: 24,
 );
-TextStyle h3Medium = GoogleFonts.inter(
+TextStyle h3Medium = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: font,
   fontSize: 20,
 );
-TextStyle h4Medium = GoogleFonts.inter(
+TextStyle h4Medium = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 16,
 );
-TextStyle h5Medium = GoogleFonts.inter(
+TextStyle h5Medium = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: font,
   fontSize: 15,
 );
 
-TextStyle h6Medium = GoogleFonts.inter(
+TextStyle h6Medium = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: font,
   fontSize: 13,
 );
-TextStyle h1SemiBold = GoogleFonts.inter(
+TextStyle h1SemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 32,
 );
-TextStyle h2SemiBold = GoogleFonts.inter(
+TextStyle h2SemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 24,
 );
-TextStyle h3SemiBold = GoogleFonts.inter(
+TextStyle h3SemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 20,
 );
-TextStyle h4SemiBold = GoogleFonts.inter(
+TextStyle h4SemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 16,
 );
-TextStyle h5SemiBold = GoogleFonts.inter(
+TextStyle h5SemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 15,
 );
 
-TextStyle h6SemiBold = GoogleFonts.inter(
+TextStyle h6SemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 13,
 );
 
-TextStyle bodyRegular = GoogleFonts.inter(
+TextStyle bodyRegular = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   fontSize: 16,
   color: font,
 );
-TextStyle body2Regular = GoogleFonts.inter(
+TextStyle body2Regular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 14,
 );
 
-TextStyle bodyBold = GoogleFonts.inter(
+TextStyle bodyBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 16,
 );
-TextStyle body2Bold = GoogleFonts.inter(
+TextStyle body2Bold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 14,
 );
-TextStyle body2SemiBold = GoogleFonts.inter(
+TextStyle body2SemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 14,
 );
-TextStyle body2Medium = GoogleFonts.inter(
+TextStyle body2Medium = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: font,
   fontSize: 14,
 );
 
-TextStyle buttonLinkLRegular = GoogleFonts.inter(
+TextStyle buttonLinkLRegular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 18,
 );
-TextStyle buttonLinkMRegular = GoogleFonts.inter(
+TextStyle buttonLinkMRegular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 16,
 );
-TextStyle buttonLinkSRegular = GoogleFonts.inter(
+TextStyle buttonLinkSRegular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 15,
 );
-TextStyle buttonLinkXSRegular = GoogleFonts.inter(
+TextStyle buttonLinkXSRegular = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 12,
 );
 
-TextStyle buttonLinkLBold = GoogleFonts.inter(
+TextStyle buttonLinkLBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 18,
 );
-TextStyle buttonLinkMBold = GoogleFonts.inter(
+TextStyle buttonLinkMBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 16,
 );
-TextStyle buttonLinkSBold = GoogleFonts.inter(
+TextStyle buttonLinkSBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 15,
 );
-TextStyle buttonLinkXSBold = GoogleFonts.inter(
+TextStyle buttonLinkXSBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w700,
   color: font,
   fontSize: 12,
 );
 
-TextStyle placeholder = GoogleFonts.inter(
+TextStyle placeholder = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 12,
 );
-TextStyle textDescription = GoogleFonts.inter(
+TextStyle textDescription = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 15,
 );
-TextStyle textDescriptionSemiBold = GoogleFonts.inter(
+TextStyle textDescriptionSemiBold = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 15,
 );
-TextStyle textDescriptionMedium = GoogleFonts.inter(
+TextStyle textDescriptionMedium = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
   color: font,
   fontSize: 15,
 );
-TextStyle textTitle = GoogleFonts.inter(
+TextStyle textTitle = GoogleFonts.poppins(
   fontWeight: FontWeight.w400,
   color: font,
   fontSize: 10,
 );
 
-TextStyle textPlace = GoogleFonts.inter(
+TextStyle textPlace = GoogleFonts.poppins(
   fontWeight: FontWeight.w600,
   color: font,
   fontSize: 18,
